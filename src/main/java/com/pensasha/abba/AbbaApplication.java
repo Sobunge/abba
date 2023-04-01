05d184fb01300455a78ef1258bca9059deffa995
Sobunge <samuelobunge@gmail.com>
2023-04-01 07:15:00 +0300

    first commit

---
+++ src/main/java/com/pensasha/abba/AbbaApplication.java
@@ -0,0 +1,13 @@
+package com.pensasha.abba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pensasha/abba/AbbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pensasha.abba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
